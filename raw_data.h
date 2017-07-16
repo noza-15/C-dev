0278ca0a5f164fd9bd12f33323e4dd6f3ab1ee04
YusukeHoriuchi <y.horiuchi@suou.waseda.jp>
2017-07-16 23:25:35 +0900

    update menu

---
+++ raw_data.h
@@ -0,0 +1,10 @@
+﻿
+#ifndef _RAW_DATA_H_
+#define _RAW_DATA_H_
+
+void drawTitle(double,double,double,int,double);
+void drawGameStart(double,double,double,int,double);
+void drawRanking(double,double,double,int,double);
+void drawExit(double,double,double,int,double);
+
+#endif // _RAW_DATA_H_
